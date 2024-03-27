b91d3cf302b5111bad860b17135f73e7e4493264
Shiinaeru <164613153+Shiinaeru-Lab@users.noreply.github.com>
2024-03-27 12:59:59 +0000

    drivers: soc: qcom: serial_num adaptep 4.19

--- drivers/soc/qcom/serial_num.c
+++ drivers/soc/qcom/serial_num.c
@@ -148,7 +148,7 @@ static int sn_fuse_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static struct of_device_id sn_fuse_match[] = {
+static const struct of_device_id sn_fuse_match[] = {
 	{.compatible = "qcom,sn-fuse"},
 	{}
 };
